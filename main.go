ea027c9a18962c69ea112e747a2f4a8b2dcf3eee
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-04-16 16:41:15 +0700

    create connection to db

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+const (
+	DB_USERNAME = "postgres"
+	DB_PASSWORD = "postgres"
+	DB_HOST     = "localhost"
+	DB_PORT     = "5432"
+	DB_DATABASE = "db_book"
+)
+
+var (
+	db  *gorm.DB
+	err error
+)
+
+func init() {
+	// postgres://postgres:postgres@localhost:5432/db_book?sslmode=disable
+	connString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
+
+	db, err = gorm.Open(postgres.Open(connString), &gorm.Config{
+		// Log to see changes
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second,
+				LogLevel:                  logger.Silent,
+				IgnoreRecordNotFoundError: true,
+				Colorful:                  true,
+			},
+		),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+}
